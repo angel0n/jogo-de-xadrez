b2df683f2b8b08d708fd3a5f94992729ff56a8c1
Angelon <angelon.gabriel@gmail.com>
2022-04-07 17:34:18 -0300

    Inicio da movimentações das peças, sem validação de movimento

--- ChessEngine.py
+++ ChessEngine.py
@@ -1,12 +1,12 @@
-# this class is responsible for storing all the information about the current state of a chess game. It will also be 
-# responsible for determining the valid moves at the current wstate. It will also keep a move log 
+# esta classe é responsável por armazenar todas as informações sobre o estado atual de um jogo de xadrez.
+# Ele também será responsável por determinar os movimentos válidos no wstate atual. Ele também manterá um registro de movimento.
 
 class GameState():
     def __init__(self):
-        #board is an 8X8 2d list, each element of the list has 2 characters. 
-        #The first character represents the color of the piece 'b' or 'w'
-        #The second character represents the type of the piece 'K', 'Q', 'R', 'B', 'N' or 'P'
-        #"--" represents an empty space with no piece 
+        #placa é uma lista 2d 8X8, cada elemento da lista tem 2 caracteres.
+        # O primeiro caractere representa a cor da peça 'b' ou 'w'
+        #O segundo caractere representa o tipo da peça 'K', 'Q', 'R', 'B', 'N' ou 'P'
+        #"--" representa um espaço vazio sem peça
         self.board = [
             ["bR","bN","bB","bQ","bK","bB","bN","bR"],
             ["bP","bP","bP","bP","bP","bP","bP","bP"],
@@ -19,3 +19,27 @@ class GameState():
         ]
         self.whiteToMove = True
         self.moveLog = []
+        
+
+class Moves():
+    
+    # maps keys to values 
+    # key: value 
+    ranksToRows = {"1":7,"2":6,"3":5,"4":4,
+                   "5":3,"6":2,"7":1,"8":0}
+    rowsToRanks = {v:k for k,v in ranksToRows.items()}
+    filesToCols = {"a":0,"b":1,"c":2,"d":3,
+                   "e":4,"f":5,"g":6,"h":7}
+    colsToFiles = {v:k for k,v in filesToCols.items()}
+    
+    
+    
+    def __init__(self,startSq,endSq,board):
+        self.startRow = startSq[0]
+        self.startColumn = startSq[1]
+        self.endRow = endSq[0]
+        self.endColumn = endSq[1]
+        self.pieceMoved = board[self.startRow][self.startColumn] #peça que foi movida
+        self.pieceCaptured = board[self.endRow][self.endColumn] #peça que foi capturada/destino da peça movida
+        
+        
--- ChessMain.py
+++ ChessMain.py
@@ -1,47 +1,67 @@
-# this is our main driver file. it will be responsible for hendling user input and displaying the current GameStae object
+# este é o nosso arquivo de driver principal. ele será responsável por manipular a entrada do usuário e exibir o objeto GameStae atual
 
 import pygame
 import ChessEngine
 
+#definiçoes de tamanho
 WIDTH = HEIGHT = 512
 DIMENSION = 8
 SQ_SIZE = HEIGHT // DIMENSION
 MAX_FPS = 15 
 IMAGES ={}
 
+#carrega as imagens
 def loadImage():
     pieces = ['wP','wR','wN','wB','wK','wQ','bP','bR','bN','bB','bK','bQ']
     for piece in pieces:
         IMAGES[piece] = pygame.transform.scale(pygame.image.load('images/'+piece+'.png'), (SQ_SIZE,SQ_SIZE) )
- 
+
+#metodo principal, executa o programa
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((WIDTH,HEIGHT))
+    screen = pygame.display.set_mode((WIDTH,HEIGHT)) #seta a tela
     clock = pygame.time.Clock()
-    screen.fill(pygame.Color("white"))
+    screen.fill(pygame.Color("white")) #pinta a tela de branco
     gs = ChessEngine.GameState()
-    loadImage( )    
-    running = True
+    loadImage()    
+    running = True #flag para controlar o loop principal
+    sqSelected = () #no inicialmente nenhum quadrado está selecionado, keep track of this last click of the user (tuple:(row,column))
+    playerClicks = [] #keep track of the clicks (two tuples: [(6,4),(4,4)])
     while running:
-        for e in pygame.event.get():
-            if e.type == pygame.QUIT:
+        for e in pygame.event.get(): #loop para capturar eventos
+            if e.type == pygame.QUIT: #evento para fechar a janela  
                 running = False
+            elif e.type == pygame.MOUSEBUTTONDOWN: #evento para pegar quando o mouse é clicado
+                location = pygame.get_mouse_pos() # pega a posição do mouse
+                column = location[0]//SQ_SIZE
+                row = location[1]//SQ_SIZE 
+                if sqSelected == (row,column):
+                    sqSelected = () # deselect
+                    playerClicks = [] #clear player clicks
+                else:
+                    sqSelected = (row,column) #select
+                    playerClicks.append(sqSelected) # add to player clicks
+                if len(playerClicks) == 2: #verifica se o usuário clicou duas vezes
+                    d=4
+                
         drawGameState(screen, gs)
         clock.tick(MAX_FPS)
         pygame.display.flip()
 
-
-def drawGameState(screen,gs):
+#desenha o tabuleiro
+def drawGameState(screen,gs): 
     drawBoard(screen)
     drawPiece(screen,gs.board)
-    
-def drawBoard(screen):
+
+#desenha os quadrados do tabuleiro 
+def drawBoard(screen): 
    colors = [pygame.Color("white"), pygame.Color("gray")]
    for row in range(DIMENSION):
        for columns in range(DIMENSION):
            color = colors[((row+columns) % 2)]
            pygame.draw.rect(screen, color, pygame.Rect(columns*SQ_SIZE,row*SQ_SIZE,SQ_SIZE,SQ_SIZE)) 
 
+#desenha as peças do tabuleiro
 def drawPiece(screen,board):
     for row in range(DIMENSION):
         for columns in range(DIMENSION):
